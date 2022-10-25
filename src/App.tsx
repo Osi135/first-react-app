09737076cb9ef3ab199ded999f0d8b6f0e76c3de
Osi135 <oscar.horn.roostami@outlook.com>
2022-10-25 17:56:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        You spin me right round, baby<br/>
+right round like a record, baby<br/>
+Right round round round<br/>
+You spin me right round, baby<br/>
+Right round like a record, baby<br/>
+Right round round round
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
